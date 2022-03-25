e7db81880ef8405cb969cf4af9ac695b020ac658
jrtanmoy <roy28301@gmail.com>
2022-03-25 21:31:45 +0600

    header & background set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello react</h1>
+    <div className='body'>
+      <Header></Header>
+      {/* <Shop></Shop> */}
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div  className='header'>
+            <h1>Guiter Heaven</h1>
+            <h2>Choose Your Guiter</h2>
+        </div>
+    );
+};
+
+export default Header;
